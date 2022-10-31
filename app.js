aee1842e905cad476a0d458d2278a85121fe2cf9
devilb2103 <devbhanushali21@gmail.com>
2022-10-31 11:03:12 +0530

    goals

--- app.js
+++ app.js
@@ -13,4 +13,37 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
+
+/**
+ *
+ * all routes here
+ *
+ * (((AUTH)))
+ * - - POST - -
+ * 1) Create user document entries on user registration or google/github login (uid is pk, email)
+ *
+ * (((EVENT ADDITION)))
+ * - - POST - -
+ * 1) Check if user calling add event has role "event-manager" and only then allow
+ *
+ * - - POST - -
+ * 2) Add event entry to document with sub-attricbutes -->
+ *                       (event name, event date, start time, end time, number of participants, participants -> (uid, email))
+ *
+ * - - GET / POST / PUT - -
+ * 3) Enroll for event
+ *
+ * 4) De-Enroll for event
+ *
+ * 4) Modify event details
+ *
+ * (((NEWS COLLECTION)))
+ * - - GET - -
+ * 1) Get news data off firestore
+ *
+ * - - POST - -
+ * 1) Scrape data off new sites filtered by tech
+ * 2) POST data to firestore
+ */
+
 app.listen(port, () => console.log(`port ${port}`));
